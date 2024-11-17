ba3182aed325f61792ad793e115abe2e7833d7dd
Abhishek Bagal <110403133+AbhishekBagal9@users.noreply.github.com>
2024-11-17 07:04:23 -0800

    Added the Habbit add section
    work on the light and dark mode and
    add the content of habbit section

--- script.js
+++ script.js
@@ -1,9 +1,46 @@
-let habits = document.querySelectorAll('.habbitbtn')
 
+//selectors
+let habits = document.querySelectorAll('.habbitbtn')
+let themebtn = document.querySelector(".theme")
+
+//storage obj and the saveTheme Method..
+const Storage = {
+  saveTheme(value) {
+    localStorage.setItem('Habbitapp.theme', `${value}`)
+  },
+  checkTheme(){
+    return localStorage.getItem('Habbitapp.theme')
+  }
+  
+}
+
+//Functions
+function themeaction(){
+  themebtn.classList.toggle("dark")
+  const root = document.querySelector(":root");
+  root.classList.toggle("dark")
+ 
+  themebtn.classList.contains('dark') ? Storage.saveTheme('dark') : Storage.saveTheme('')
+}
+
+
+//Events
+themebtn.addEventListener("click",function(){
+  themeaction()
+})
 
 habits.forEach((habit) => {
     habit.addEventListener("click",function(){
       habit.classList.toggle("completed")
     })
 })
+
+window.addEventListener('DOMContentLoaded',()=>{
+  const theme = Storage.checkTheme();
+  if(theme === 'dark'){
+    themeaction();
+  }
+ 
+})
+
 
